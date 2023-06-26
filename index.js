d21ec4ebae5257956b7e68df9fe73da87eccad9f
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2023-06-26 17:47:35 -0400

    working patch reqs and clicks

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 const loginRoute = require('./Routes/login');
 const registerRoute = require('./Routes/register');
+const UserModel = require('./Models/User');
 
 require('dotenv').config();
 
@@ -19,6 +20,27 @@ mongoose.connect(URI);
 
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
+
+app.patch('/userClicks/:userId', async (req, res) => {
+  const { userId } = req.params;
+  const { count } = req.body;
+
+  try {
+    const user = await UserModel.findByIdAndUpdate(
+      userId,
+      { clicks: count + 1 },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    console.log(req.body);
+    res.send(user);
+  } catch (error) {
+    console.error('Failed to update user count: ', error);
+    res.status(500).send({ error: 'Failed to update user count' });
+  }
+});
 
 app.listen(PORT_NUMBER, () => {
   console.log(`CONNECTED TO ${PORT_NUMBER}`);
